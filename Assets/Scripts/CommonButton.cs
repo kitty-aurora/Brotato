bb043c05263daaf0bccb6c3e0beaf74e2d965490
miao <1192247438@qq.com>
2025-10-23 17:22:45 +0800

    Initial commit

---
+++ Assets/Scripts/CommonButton.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class CommonButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Image _image;
+    private TextMeshProUGUI _text;
+
+    private void Awake()
+    {
+        _image = GetComponent<Image>();
+        _text = GetComponentInChildren<TextMeshProUGUI>();
+    }
+    
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _image.color = Color.white;
+        _text.color = Color.black;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _image.color = Color.black;
+        _text.color = Color.white;
+    }
+}
