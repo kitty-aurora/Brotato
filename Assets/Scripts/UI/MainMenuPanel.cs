087015ac175ab4f3c18bdecacfe84694f260be9d
miao <1192247438@qq.com>
2025-10-26 22:54:42 +0800

    角色信息

--- Assets/Scripts/UI/MainMenuPanel.cs
+++ Assets/Scripts/UI/MainMenuPanel.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class black : MonoBehaviour
@@ -7,11 +9,18 @@ public class black : MonoBehaviour
     public Button settingButton;
     public Button progressButton;
     public Button exitButton;
-    
+
+
+    private void Awake()
+    {
+        startButton = GameObject.Find("StartButton").GetComponent<Button>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startButton.onClick.AddListener((() => { SceneManager.LoadScene(1); }
+            ));
     }
 
     // Update is called once per frame
