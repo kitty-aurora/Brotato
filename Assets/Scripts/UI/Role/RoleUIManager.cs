aac4fd269b9f751a49eee27ced2d4dd2ff1dfab5
miao <1192247438@qq.com>
2025-11-03 19:57:54 +0800

    武器UI

--- Assets/Scripts/UI/Role/RoleUIManager.cs
+++ Assets/Scripts/UI/Role/RoleUIManager.cs
@@ -7,24 +7,24 @@ using UnityEngine.UI;
 
 public class RoleUIManager : MonoBehaviour
 {
-    [Header("角色卡片预制体")]
-    public GameObject rolePrefab;
+    [Header("角色卡片预制体")] public GameObject rolePrefab;
 
-    [Header("角色列表容器")]
-    public Transform roleListParent;
-    
-   
+    [Header("角色列表容器")] public Transform roleListParent;
+
+
     private TextMeshProUGUI _titleText;
-    
     private GameObject _recordPanel;
     private GameObject _weaponSelectPanel;
-    
+
+    private GameObject _roleList;
+    private GameObject _weaponList;
+
     // 角色数据
     private List<RoleData> roleList = new();
-    
+
     // 武器数据
     private List<WeaponData> weaponList = new();
-    
+
     private void Awake()
     {
         // 从 JSON 文件加载角色数据
@@ -33,7 +33,7 @@ public class RoleUIManager : MonoBehaviour
         // 加载武器数据
         var weaponTextAsset = Resources.Load<TextAsset>("Data/weapon");
         weaponList = JsonConvert.DeserializeObject<List<WeaponData>>(weaponTextAsset.text);
-        
+
         _recordPanel = GameObject.Find("RoleRecordPanel");
         _weaponSelectPanel = GameObject.Find("WeaponSelectPanel");
         _titleText = GameObject.Find("Title").GetComponent<TextMeshProUGUI>();
@@ -45,7 +45,7 @@ public class RoleUIManager : MonoBehaviour
         {
             // 1. 实例化卡片
             var roleGO = Instantiate(rolePrefab, roleListParent);
-            
+
             // 2. 传入数据
             var roleSelect = roleGO.GetComponent<RoleUISelect>();
             roleSelect.SetRoleData(roleData);
@@ -54,10 +54,14 @@ public class RoleUIManager : MonoBehaviour
 
     public void ShowWeaponPanel()
     {
-        _recordPanel.SetActive(false);
-       // _roleList
-        _weaponSelectPanel.SetActive(true);
-      //  _weaponList
         _titleText.text = "武器选择";
+        
+        _recordPanel.SetActive(false);
+        _roleList.SetActive(false);
+        
+        _weaponSelectPanel.SetActive(true);
+        _weaponList.SetActive(true);
+        
+       
     }
 }
